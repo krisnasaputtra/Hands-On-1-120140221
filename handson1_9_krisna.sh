d891f7d50e1bafe771227572ce2d650c8391aaac
krisnasaputtra <krisna.saputra031002@gmail.com>
2022-03-06 01:58:40 +0700

    fixed

--- handson1_9_krisna.sh
+++ handson1_9_krisna.sh
@@ -2,5 +2,5 @@ echo -n "Masukkan nama file: "
 read coba9
 echo -n "Masukkan key: "
 read keys
-grep -vwiE "($keys)" $coba9 > hasil_coba10
-cat hasil_coba10
+grep -vwiE "($keys)" $coba9 > hasil_coba9
+cat hasil_coba9
